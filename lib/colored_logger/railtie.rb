4a2cb03fe1700ef4a38c706ae37ae816546067e1
Lawrence Norton <lawrence@lawrencenorton.com>
2014-03-19 19:40:35 -0400

    updated logger

--- lib/colored_logger/railtie.rb
+++ lib/colored_logger/railtie.rb
@@ -3,7 +3,7 @@ require 'rails/railtie'
 module ColoredLogger
     class Railtie < Rails::Railtie
         initializer "colored_logger.initialize" do |app|
-            app.config.logger = ActiveSupport::Logger.new(Rails.root.join('log','#{Rails.env}.log'))
+            app.config.logger = ActiveSupport::Logger.new(Rails.root.join('log',"#{app.class.parent_name}.log"))
         end
     end
 end
