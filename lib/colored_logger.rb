acef05b63f474fd68f2d6d9189f5ca31c235b00d
Lawrence Norton <lawrence@lawrencenorton.com>
2014-03-20 14:16:41 -0400

    updated

--- lib/colored_logger.rb
+++ lib/colored_logger.rb
@@ -3,48 +3,53 @@ require "colored_logger/core_ext"
 require 'colored_logger/railtie' if defined?(Rails)
 
 module ColoredLogger
+  #  :black, :red, :green, :yellow, :blue, :magenta, :cyan, :white
 
   COLORS = {
-    'black'   => 30,
-    'red'     => 31,
-    'green'   => 32,
-    'yellow'  => 33,
-    'blue'    => 34,
-    'magenta' => 35,
-    'cyan'    => 36,
-    'white'   => 37
+    :reset         => 0,
+    :black        => 30,
+    :red           => 31,
+    :green        => 32,
+    :yellow        => 33,
+    :blue          => 34,
+    :magenta   => 35,
+    :cyan          => 36,
+    :white         => 37
   }
 
-  BACKGROUND_COLORS = {
-    'black'    => 40,
-    'red'     => 41,
-    'green'   => 42,
-    'yellow'  => 43,
-    'blue'    => 44,
-    'magenta' => 45,
-    'cyan'    => 46,
-    'white'   => 47
+  BGCOLORS = {
+    :black    => 40,
+    :red     => 41,
+    :green   => 42,
+    :yellow  => 43,
+    :blue    => 44,
+    :magenta => 45,
+    :cyan    => 46,
+    :white   => 47
   }
 
   EXTRAS = {
-    'clear'     => 0,
-    'bold'      => 1,
-    'dark'      => 2,
-    'italic'      => 3,
-    'underline'   => 4,
-    'reversed'    => 7
+    :clear     => 0,
+    :bold      => 1,
+    :dark      => 2,
+    :italic    => 3,
+    :underline   => 4,
+    :reversed   => 7
   }
+
   class ActiveSupport::Logger::SimpleFormatter
-    SEVERITY_TO_TAG_MAP     = {'DEBUG'=>'nom', 'INFO'=>'fyi', 'WARN'=>'hmm', 'ERROR'=>'wtf', 'FATAL'=>'omg', 'UNKNOWN'=>'???'}
-    SEVERITY_TO_COLOR_MAP   = {'DEBUG'=>'0;37;45', 'INFO'=>'32', 'WARN'=>'33', 'ERROR'=>'31', 'FATAL'=>'31', 'UNKNOWN'=>'37'}
-    USE_HUMOROUS_SEVERITIES = true
+
+    SEVERITY_TO_COLOR_MAP   = {
+      'DEBUG'=>"#{COLORS[:reset]};#{COLORS[:white]};#{BGCOLORS[:magenta]};#{EXTRAS[:bold]}",
+      'INFO'=>"#{COLORS[:green]}",
+      'WARN'=>"#{COLORS[:yellow]}",
+      'ERROR'=>";#{COLORS[:magenta]};#{BGCOLORS[:white]}",
+      'FATAL'=>";#{COLORS[:red]}",
+      'UNKNOWN'=>"#{COLORS[:white]}"
+    }
 
     def call(severity, time, progname, msg)
-      if USE_HUMOROUS_SEVERITIES
-        formatted_severity = sprintf("%-3s",SEVERITY_TO_TAG_MAP[severity])
-      else
-        formatted_severity = sprintf("%-5s",severity)
-      end
+      formatted_severity = sprintf("%-4s",severity)
 
       formatted_time = time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)
       color = SEVERITY_TO_COLOR_MAP[severity]
--- lib/colored_logger/railtie.rb
+++ lib/colored_logger/railtie.rb
@@ -1,9 +1,9 @@
 require 'rails/railtie'
 
 module ColoredLogger
-    class Railtie < Rails::Railtie
-        initializer "colored_logger.initialize" do |app|
-            app.config.logger = ActiveSupport::Logger.new(Rails.root.join('log',"#{Rails.env}.log"))
-        end
+  class Railtie < Rails::Railtie
+    initializer "colored_logger.initialize" do |app|
+      app.config.logger = ActiveSupport::Logger.new(STDOUT)
     end
+  end
 end
